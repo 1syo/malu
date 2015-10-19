3ea688062823e4081bd9ce718961051546583de1
TAKAHASHI Kazunari <takahashi@1syo.net>
2015-10-19 16:23:50 +0900

    Add request

---
+++ lib/request.rb
@@ -0,0 +1,20 @@
+class Request
+  extend Forwardable
+  def_delegators :@_request, :path_info
+
+  def initialize(env:)
+    @_request = Rack::Request.new(env)
+  end
+
+  def headers
+    {
+      'User-Agent' => @_request.user_agent,
+      'Content-Type' => @_request.content_type,
+      'Accept' => @_request.env['HTTP_ACCEPT']
+    }
+  end
+
+  def body
+    @_body ||= @_request.body.read
+  end
+end
