81b0d0453f6703f89e21250034cd8c4fda28e148
TAKAHASHI Kazunari <takahashi@1syo.net>
2015-10-23 23:15:32 +0900

    Remove space form key

--- spec/lib/store_spec.rb
+++ spec/lib/store_spec.rb
@@ -11,55 +11,55 @@ describe Store do
 
   describe '#add' do
     it 'valid' do
-      assert { @store.add('POST /hook_1', 'https://example.com?a=c') }
+      assert { @store.add('POST/hook_1', 'https://example.com?a=c') }
     end
 
     it 'invalid method' do
-      assert { @store.add('HOO /hook', 'https://example.com') == false }
+      assert { @store.add('HOO/hook', 'https://example.com') == false }
     end
 
     it 'invalid url' do
-      assert { @store.add('POST /hook', 'https:||a') == false }
+      assert { @store.add('POST/hook', 'https:||a') == false }
     end
 
     it 'invalid url' do
-      assert { @store.add('POST /hook', 'https') == false }
+      assert { @store.add('POST/hook', 'https') == false }
     end
   end
 
   describe '#get' do
     before do
-      @store.add('POST /hook1', 'https://a.example.com?b=c')
-      @store.add('POST /hook1', 'https://x.example.com?y=z')
-      @store.add('POST /hook2', 'https://1.example.com?2=3')
+      @store.add('POST/hook1', 'https://a.example.com?b=c')
+      @store.add('POST/hook1', 'https://x.example.com?y=z')
+      @store.add('POST/hook2', 'https://1.example.com?2=3')
     end
 
     it 'include urls' do
-      assert { @store.get('POST /hook1').include?('https://a.example.com?b=c') }
-      assert { @store.get('POST /hook1').include?('https://x.example.com?y=z') }
-      assert { @store.get('POST /hook1').include?('https://1.example.com?2=3') == false }
+      assert { @store.get('POST/hook1').include?('https://a.example.com?b=c') }
+      assert { @store.get('POST/hook1').include?('https://x.example.com?y=z') }
+      assert { @store.get('POST/hook1').include?('https://1.example.com?2=3') == false }
     end
 
     it 'not exists' do
-      assert { @store.get('POST /hook3') == [] }
+      assert { @store.get('POST/hook3') == [] }
     end
   end
 
   describe '#del' do
     before do
-      @store.add('POST /hook1', 'https://a.example.com?b=c')
+      @store.add('POST/hook1', 'https://a.example.com?b=c')
     end
 
     it 'valid' do
-      assert { @store.del('POST /hook1') }
+      assert { @store.del('POST/hook1') }
     end
   end
 
   describe '.all' do
     before do
-      @store.add('POST /hook1', 'https://a.example.com?b=c')
-      @store.add('POST /hook1', 'https://x.example.com?y=z')
-      @store.add('POST /hook2', 'https://1.example.com?2=3')
+      @store.add('POST/hook1', 'https://a.example.com?b=c')
+      @store.add('POST/hook1', 'https://x.example.com?y=z')
+      @store.add('POST/hook2', 'https://1.example.com?2=3')
     end
 
     it 'have 2' do
