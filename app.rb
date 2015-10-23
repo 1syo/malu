879363c38c03de6fd22e8bc6f5680b02bfca2685
TAKAHASHI Kazunari <takahashi@1syo.net>
2015-10-23 22:38:39 +0900

    Refactor app

--- app.rb
+++ app.rb
@@ -1,19 +1,23 @@
 require 'bundler/setup'
-Bundler.require(:default, ENV['RACK_ENV'])
+Bundler.require(:default, ENV['RACK_ENV'] || :development)
 
 $LOAD_PATH << File.expand_path('../lib', __FILE__)
 require 'postman'
 require 'request'
-require 'route'
+require 'routing_set'
+require 'store'
 
 class App
   def self.call(env)
     request = Request.new(env: env)
 
-    Route.new.get(request.path_info).each do |url|
+    routing_set = RoutingSet.new
+    routing_set.routes(method: request.method, path: request.path).each do |url|
       Postman.new(url: url, request: request).post
     end
 
-    [200, {'Content-Type' => 'text/plain'}, ['OK']]
+    [200, { 'Content-Type' => 'text/plain' }, ['OK']]
+  rescue
+    [500, { 'Content-Type' => 'text/plain' }, ['ERR']]
   end
 end
